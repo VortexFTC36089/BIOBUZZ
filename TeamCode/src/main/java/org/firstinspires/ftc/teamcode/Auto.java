26bcd49a711be0201915bee123d3efaf98d371a8
Hrithik0311 <hraha0311@gmail.com>
2026-08-31 18:06:46 -0500

    New Branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -3,18 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-/**
- * A starter autonomous program. It uses time, not cameras or path following.
- *
- * Change the numbers, then test on the field.
- *   robot.drive(forward, strafe, turn)
- *   sleep(milliseconds)   1000 = 1 second
- *
- * Example:
- *   robot.drive(0.5, 0, 0);   // drive forward at half speed
- *   sleep(1000);              // for 1 second
- *   robot.stop();
- */
+
 @Autonomous(name = "Simple Auto", group = "Competition")
 public class Auto extends LinearOpMode {
 
